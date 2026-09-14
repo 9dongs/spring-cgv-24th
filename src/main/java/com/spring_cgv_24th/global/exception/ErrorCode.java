d36c76e9b4d719f30971e2a4bc0cdeae74f69fe1
9dongs <144160177+9dongs@users.noreply.github.com>
2026-09-14 19:43:43 +0900

    feat: 전역 예외 처리 체계 구현

---
+++ src/main/java/com/spring_cgv_24th/global/exception/ErrorCode.java
@@ -0,0 +1,15 @@
+package com.spring_cgv_24th.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 오류가 발생했습니다."),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다.");
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
---
+++ src/main/java/com/spring_cgv_24th/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.spring_cgv_24th.global.exception;
+
+import com.spring_cgv_24th.global.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(com.spring_cgv_24th.global.exception.CustomException.class)
+    public ResponseEntity<ApiResponse<Void>> handleCustomException(com.spring_cgv_24th.global.exception.CustomException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(ApiResponse.onFailure(errorCode.getCode(), errorCode.getMessage(), null));
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Void>> handleValidationException(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getDefaultMessage())
+                .findFirst()
+                .orElse(ErrorCode.BAD_REQUEST.getMessage());
+        return ResponseEntity
+                .status(ErrorCode.BAD_REQUEST.getHttpStatus())
+                .body(ApiResponse.onFailure(ErrorCode.BAD_REQUEST.getCode(), message, null));
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
+        log.error("처리되지 않은 서버 오류가 발생했습니다.", e);
+
+        return ResponseEntity
+                .status(ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus())
+                .body(ApiResponse.onFailure(
+                        ErrorCode.INTERNAL_SERVER_ERROR.getCode(),
+                        ErrorCode.INTERNAL_SERVER_ERROR.getMessage(), null));
+    }
+}
